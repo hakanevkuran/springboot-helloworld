2cb0e7f51baf497f5adcbaa796ec9ae21dc0383a
Kubilay <32797624+kubilaykaptanoglu@users.noreply.github.com>
2019-04-15 19:06:28 +0300

    Update HomeControllerTest.java

--- src/test/java/com/gazgeek/helloworld/HomeControllerTest.java
+++ src/test/java/com/gazgeek/helloworld/HomeControllerTest.java
@@ -28,7 +28,7 @@ public class HomeControllerTest {
     public void helloWorld() {
         getRequest("/")
             .assertStatusCode(OK);
-    //        .assertResponseBody("Hello from Kaptanoglu!");
+            .assertResponseBody("Hello from Kaptanoglu!");
     }
 
     private HelloWorldResponse getRequest(String uri) {
